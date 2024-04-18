42595a673dfe982b2235826b5fd9d2455875094c
suhasnisingh <165236685+suhasnisingh@users.noreply.github.com>
2024-04-18 02:16:21 +0000

    complete

--- app.rb
+++ app.rb
@@ -2,8 +2,49 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square_new)
+end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/square/results") do
+  @number = params.fetch("number").to_i
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root_new)
+end
+
+get("/square_root/results") do
+  @number = params.fetch("number").to_i
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:payment_new)
+end
+
+get("/payment/results") do
+  @apr = params.fetch("apr").to_f
+  @years = params.fetch("years").to_f
+  @pv = params.fetch("pv").to_f
+  @numerator = (@apr / (100 * 12)) * @pv
+  @denominator = 1 - ((1-@apr)**(-12 * @years))
+  @result = @numerator / @denominator
+  erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:random_new)
+end
+
+get("/random/results") do
+  @min = params.fetch("min").to_f
+  @max = params.fetch("max").to_f
+  @result = rand(@min..@max)
+
+  erb(:random_results)
 end
